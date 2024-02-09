91c2bf090efe386b5460178e195d49d1d6d167ed
a-gafron <gafron-alex@gmx.de>
2024-02-09 12:55:28 +0100

    Created Coin class with nr. and weight

---
+++ Coin.java
@@ -0,0 +1,14 @@
+public class Coin {
+    private int nrOfTheCoin = 0;
+    private int weightOfTheCoin = 0;
+
+
+
+
+
+    public Coin (int nrOfTheCoin_, int weightOfTheCoin_){
+        this.nrOfTheCoin = nrOfTheCoin_;
+        this.weightOfTheCoin = weightOfTheCoin_;
+    }
+
+}

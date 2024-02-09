bf3c22867441ba92288b86c284bc581f5d3e0ca7
a-gafron <gafron-alex@gmx.de>
2024-02-09 13:03:54 +0100

    Getter and Setter for private nr, weight

--- Coin.java
+++ Coin.java
@@ -2,13 +2,22 @@ public class Coin {
     private int nrOfTheCoin = 0;
     private int weightOfTheCoin = 0;
 
-
-
-
-
     public Coin (int nrOfTheCoin_, int weightOfTheCoin_){
         this.nrOfTheCoin = nrOfTheCoin_;
         this.weightOfTheCoin = weightOfTheCoin_;
     }
 
+    public int getNrOfTheCoin() {
+        return nrOfTheCoin;
+    }
+
+    public void setNrOfTheCoin(int nrOfTheCoin){
+        this.nrOfTheCoin = nrOfTheCoin;
+    }
+    public int getWeightOfTheCoin(){
+        return weightOfTheCoin;
+    }
+    public void setWeightOfTheCoin(int weightOfTheCoin){
+        this.weightOfTheCoin = weightOfTheCoin;
+    }
 }

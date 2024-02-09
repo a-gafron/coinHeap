ce5b7b955a192fa8f8381fe9a2bde11062b84c8b
a-gafron <gafron-alex@gmx.de>
2024-02-09 13:26:18 +0100

    Method to makeTheAmountDivisible

--- Utility.java
+++ Utility.java
@@ -1,16 +1,29 @@
 public class Utility {
 
-    public static int currentAmountOfCoins = 19863;
+    public static int currentAmountOfCoins = 19863; //create a conversation, where per scanner the amount could get set
     public static int minAmountOfCoins = 0;
     public static int random_int = (int)Math.floor(Math.random() * (currentAmountOfCoins - minAmountOfCoins + 1) + minAmountOfCoins);
     public static int counterOfWeighing = 0;
-
     public static int subtractRest = 0;
     public static int endFirstThird = 0;
     public static int endSecondThird = 0;
 
+    public static int actualCountOfCoins;
     public static Heap aFreshHeap;
     public static Heap firstThird;
     public static Heap secondThird;
     public static Heap thirdThird;
+
+    public static int makeTheAmountDivisible (){
+        int rest = 0;
+        if (currentAmountOfCoins % 3 == 0) {
+            rest = 0;
+        } else if (currentAmountOfCoins % 3 == 1) {
+            rest = 1;
+        } else if (currentAmountOfCoins % 3 == 2) {
+            rest = 2;
+        }
+        //here is an exception required
+        return rest;
+    }
 }
